430f0cff4acc09a43deb4a089d0fee828bed6e5b
noeyadd <wkdekdus6778@naver.com>
2024-04-04 16:36:04 +0900

    fix:view 경로 수정

--- Instagram/liongram_Prj/posts/views.py
+++ Instagram/liongram_Prj/posts/views.py
@@ -3,7 +3,7 @@ from .models import Post
 
 def list(request):
     posts = Post.objects.all().order_by('-created_at')
-    return render(request, 'list.html', {'posts':posts})
+    return render(request, 'posts/list.html', {'posts':posts})
 
 def search(request):
     data = request.GET['searchtext']
@@ -11,4 +11,4 @@ def search(request):
     # 쿼리셋 사용 (contains과 Q)
     posts = Post.objects.filter(title__contains = data) | Post.objects.filter(content__contains = data)
 
-    return render(request, 'search.html', {'posts':posts, 'data':data})
+    return render(request, 'posts/search.html', {'posts':posts, 'data':data})
