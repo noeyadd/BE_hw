4caed3aa4ffa12d65413a14efe47919b7ef41398
noeyadd <wkdekdus6778@naver.com>
2024-05-01 20:03:08 +0900

    session study

---
+++ Instagram/liongram_Prj/posts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import list, search, create, detail, update, delete
+
+urlpatterns = [
+    path('', list, name = "list"),
+    path('result/', search, name = "search"),
+    path('create/', create, name = "create"),
+    path('detail/<int:id>/', detail, name = "detail"),
+    path('update/<int:id>/', update, name = "update"),
+    path('delete/<int:id>/', delete, name = "delete"),
+]
--- Instagram/liongram_Prj/posts/views.py
+++ Instagram/liongram_Prj/posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post
 
 def list(request):
@@ -7,8 +7,44 @@ def list(request):
 
 def search(request):
     data = request.GET['searchtext']
-
     # 쿼리셋 사용 (contains과 Q)
     posts = Post.objects.filter(title__contains = data) | Post.objects.filter(content__contains = data)
-
     return render(request, 'posts/search.html', {'posts':posts, 'data':data})
+
+# CRUD - Create
+def create(request):
+    if request.method == "POST":
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+
+        post = Post.objects.create(
+            title = title,
+            content = content,
+        )
+        return redirect('list')
+    return render(request, 'posts/create.html')
+
+# CRUD - Read
+def detail(request, id):
+    post = get_object_or_404(Post, id = id)
+    # 조회수 증가
+    post.views += 1
+    post.save()
+    
+    return render(request, 'posts/detail.html', {'post' : post})
+
+# CRUD - Update
+def update(request, id):
+    post = get_object_or_404(Post, id = id)
+    if request.method == "POST":
+        post.title = request.POST.get('title')
+        post.content = request.POST.get('content')
+        post.save()
+        return redirect('detail', id)
+    return render(request, 'posts/update.html', {'post' : post})
+
+# CRUD - Delete
+def delete(request, id):
+    post = get_object_or_404(Post, id = id)
+    post.delete()
+    return redirect('list')
