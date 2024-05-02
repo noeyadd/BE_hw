e4faeb4379d1347edaa81eec2eb76adaa5b7b99b
noeyadd <wkdekdus6778@naver.com>
2024-05-02 16:29:08 +0900

    스터디과제

--- Phone/Phone_Prj/contacts/urls.py
+++ Phone/Phone_Prj/contacts/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import *
+from .views import list, search, create, detail, update, delete
 
 urlpatterns = [
-    path('', list.as_view(), name='list'),
-    path('result/', search, name='search'),
+    path('', list, name="list"),
+    path('result/', search, name="search"),
+    path('create/', create, name="create"),
+    path('detail/<int:id>/', detail, name = "detail"),
+    path('update/<int:id>/', update, name = "update"),
+    path('delete/<int:id>/', delete, name = "delete"),
 ]
--- Phone/Phone_Prj/contacts/views.py
+++ Phone/Phone_Prj/contacts/views.py
@@ -1,17 +1,54 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Phone
 
-class list(ListView):
+def list(request):
     # Phone 객체의 이름을 오름차순(이름순으로)으로 정렬
-    queryset = Phone.objects.all().order_by('name')
-    model = Phone
-    template_name = 'list.html'
-    context_object_name = 'phones'
+    phones = Phone.objects.all().order_by('name')
+    return render(request, 'contacts/list.html', {'phones':phones})
+    
 
 def search(request):
     data = request.GET['searchtext']
 
     # 쿼리셋 사용 (contains)
     phones = Phone.objects.filter(name__contains = data)
-    return render(request, 'search.html', {'phones':phones, 'data':data})
+    return render(request, 'contacts/search.html', {'phones':phones, 'data':data})
+
+# CRUD - Create
+def create(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        phone_num = request.POST.get('phone_num')
+        email = request.POST.get('email')
+
+        phones = Phone.objects.create(
+            name = name,
+            phone_num = phone_num,
+            email = email,
+        )
+        return redirect('list')
+    return render(request, 'contacts/create.html')
+
+# CRUD - Read
+def detail(request, id):
+    phone = get_object_or_404(Phone, id = id)    
+    return render(request, 'contacts/detail.html', {'phone' : phone})
+
+# CRUD - Update
+def update(request, id):
+    phone = get_object_or_404(Phone, id = id)
+    if request.method == "POST":
+        phone.name = request.POST.get('name')
+        phone.phone_num = request.POST.get('phone_num')
+        phone.email = request.POST.get('email')
+        phone.save()
+        return redirect('detail', id)
+    return render(request, 'contacts/update.html', {'phone' : phone})
+
+# CRUD - Delete
+def delete(request, id):
+    phone = get_object_or_404(Phone, id = id)
+    if request.method == "POST":
+        phone.delete()
+        return redirect('list')
+    return render(request, 'contacts/delete.html', {'phone' : phone})
